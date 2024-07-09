9affaf259e1569b2e467d80a047ccf10ab754670
jamlmao <jajamagustin0624@gmail.com>
2024-07-09 21:05:21 +0800

    add fontawesome icon nasa gc ang key

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router, RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserTie } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, FontAwesomeModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-
+  faUserTie = faUserTie; // icon 
 }
