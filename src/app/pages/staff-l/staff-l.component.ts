9ee7416e20bed02012b8d585dbbeab0de02c93a3
jamlmao <jajamagustin0624@gmail.com>
2024-07-11 13:06:38 +0800

    naadd na gawa ni lems

--- src/app/pages/staff-l/staff-l.component.ts
+++ src/app/pages/staff-l/staff-l.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-staff-l',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, RouterOutlet],
   templateUrl: './staff-l.component.html',
   styleUrl: './staff-l.component.css'
 })
