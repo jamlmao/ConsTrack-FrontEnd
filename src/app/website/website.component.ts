fccb52a12f68a6453c4ccec243a9922e9841a891
Lemuel <jameslemuel4@gmail.com>
2024-09-08 01:59:05 +0800

    website

--- src/app/website/website.component.ts
+++ src/app/website/website.component.ts
@@ -1,9 +1,16 @@
+import { HttpClient, HttpClientModule} from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouterOutlet, Router, RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { FormsModule } from '@angular/forms';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-website',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterModule ],
   templateUrl: './website.component.html',
   styleUrl: './website.component.css'
 })
