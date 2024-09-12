2eb4d628af4ee2b256ba695832cef5c471aeb00c
jamlmao <jajamagustin0624@gmail.com>
2024-09-12 17:16:21 +0800

    add autolog out for 2 mins idle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,NgZone,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet, Router, NavigationEnd  } from '@angular/router';
 import { MenuComponent } from "./pages/menu/menu.component";
 import { AdminDashboardComponent } from "./pages/components/Admin/admin-dashboard/admin-dashboard.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FilterPipe } from './filter.pipe';
-
+import { AuthService } from './pages/guard/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -16,5 +16,34 @@ import { FilterPipe } from './filter.pipe';
 })
 export class AppComponent {
   title = 'v1';
-  
+  private logoutTimer: any;
+  constructor(private router: Router, private ngZone: NgZone,private authService: AuthService) {}
+
+  ngOnInit() {
+    this.resetLogoutTimer();
+    this.setupActivityListeners();
+  }
+
+  setupActivityListeners() {
+    ['mousemove', 'keydown', 'click'].forEach(event => {
+      window.addEventListener(event, () => this.resetLogoutTimer());
+    });
+  }
+
+  resetLogoutTimer() {
+    if (this.logoutTimer) {
+      clearTimeout(this.logoutTimer);
+    }
+    this.logoutTimer = setTimeout(() => this.logout(), 120000); 
+   
+  }
+
+  logout() {
+    this.ngZone.run(() => {
+      this.authService.logout();
+    });
+  }
 }
+
+
+
--- src/app/pages/guard/auth.service.ts
+++ src/app/pages/guard/auth.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private currentUser: any; // Replace with your user model
+  private currentUser: any; 
 
-  constructor() {
-    // Initialize currentUser from localStorage or API
+  constructor(private router: Router) {
+  
     this.currentUser = JSON.parse(localStorage.getItem('user') || '{}');
   }
 
   getCurrentUser() {
     return this.currentUser;
-  }
+    }
+    
+    isAuthenticated(): boolean {
+      return !!localStorage.getItem('user');
+    }
+  
+    logout() {
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
+    }
 
-  // Add other authentication methods as needed
 }
